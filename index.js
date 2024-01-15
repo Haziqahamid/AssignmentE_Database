6d573255e31365a4fb25bc084cd9ad4fbc5bf4b1
Siti Nurhaziqah <b022210033@student.utem.edu.my>
2024-01-15 19:37:07 +0800

    Azie

--- index.js
+++ index.js
@@ -264,28 +264,16 @@ app.post('/AddStudent', async (req, res) => {
   AcademicAdministrator.AddStudent (req, res);
 })
 
-app.listen(port, () => {
-console.log(`Example app listening on port ${port}`)
+app.post('/AddLecturer', async (req, res) => {
+	console.log(req.body);
+  AcademicAdministrator.AddStudent (req, res);
 })
 
-/*const jwt = require('jsonwebtoken');
-function generateAccessToken(payload) {
-	return jwt.sign(payload, "Assignment-GroupE", { expiresIn: '1h' });
-}
+app.post('/StudentList', async (req, res) => {
+	console.log(req.body);
+  AcademicAdministrator.AddStudent (req, res);
+})
 
-function verifyToken(req, res, next) {
-	const authHeader = req.headers['authorization']
-	const token = authHeader && authHeader.split(' ')[1]
-
-	if (token == null) return res.sendStatus(401)
-
-	jwt.verify(token, "Assignment-GroupE", (err, user) => {
-		console.log(err)
-
-		if (err) return res.sendStatus(403)
-
-		req.user = user
-
-		next()
-	})
-}*/
+app.listen(port, () => {
+console.log(`Example app listening on port ${port}`)
+});
