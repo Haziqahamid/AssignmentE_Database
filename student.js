23ea47d4a8ffea081fbd226ff582c4f740169017
Siti Nurhaziqah <b022210033@student.utem.edu.my>
2025-01-07 20:42:25 +0800

    Azie

--- student.js
+++ student.js
@@ -45,21 +45,6 @@ exports.recordAttendance = function (req, res) {
     });
 }
 
-/*exports.attendanceDetails = async function (req, res) {
-  console.log(req.body);
-  const { StudentID } = req.params;
-
-  try {
-    const okay = await client.db("Assignment").collection('Attendance').find({ "StudentID": { $eq: StudentID } }).toArray();
-    res.status(200).send(okay);
-  }
-
-  catch (err) {
-    console.error(err);
-    res.status(500).send('Error fetching attendance details');
-  }
-};*/
-
 exports.attendanceDetails = async function (req, res) {
   console.log(req.body);
   const StudentID = req.params.StudentID;
