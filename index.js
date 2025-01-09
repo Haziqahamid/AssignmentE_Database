26dc2f2effbd34f5c15e774248d2ea62d6611632
Siti Nurhaziqah <b022210033@student.utem.edu.my>
2025-01-09 21:58:58 +0800

    Azie

--- index.js
+++ index.js
@@ -51,8 +51,8 @@ app.use(globalLimiter);
 
 // Custom Rate Limiting for Login Endpoint
 const loginLimiter = rateLimit({
-  windowMs: 5 * 60 * 1000, // 5 minutes
-  max: 5, // Limit each IP to 5 login attempts per 5 minutes
+  windowMs: 30 * 60 * 1000, // 5 minutes
+  max: 3, // Limit each IP to 5 login attempts per 5 minutes
   message: 'Too many login attempts. Please try again after 5 minutes.',
 });
 
