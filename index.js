61474c2046a1f7697603a8f67e6bf8c244211704
Siti Nurhaziqah <b022210033@student.utem.edu.my>
2025-01-21 15:42:22 +0800

    Azie

--- index.js
+++ index.js
@@ -39,8 +39,8 @@ app.use(express.json())
 
 // Global Rate Limiting
 const globalLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 5, // Limit each IP to 5 requests per 15 minutes
+    windowMs: 60 * 1000, // 1 minutes
+    max: 5, // Limit each IP to 5 requests per minutes
     message: 'Too many requests from this IP, please try again after 15 minutes.',
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
